38a37f7225f46c546dccca58e7b1d8bfc72533a5
Shoji Moto <shmoto1@gmail.com>
2016-12-13 08:37:01 -0500

    One way old SAT

--- satactbot.py
+++ satactbot.py
@@ -10,10 +10,13 @@ REPLY_TEMP = "beep boop\n\nThe equivalent " #ACT/SAT
 REPLY_TEMP2 = " score to your " #ACT/SAT
 REPLY_TEMP3 = " score is " #score
 REPLY_TEMP4 = ".\n\n***\n\n^Data ^was ^provided ^by ^Collegeboard's ^Concordance ^tables ^last ^updated ^May ^9, ^2016 ^| ^Created ^by ^/u/Pianobin"
+REPLY_TEMP5 = ". The equivalent "
 
 ACTscores = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
 
 SATscores = [560, 630, 720, 760, 810, 860, 900, 940, 980, 1020, 1060, 1100, 1130, 1160, 1200, 1240, 1280, 1310, 1350, 1390, 1420, 1450, 1490, 1520, 1560, 1600]
+
+oldSATscores = [1610, 1620, 1640, 1650, 1670, 1680, 1700, 1710, 1730, 1750, 1760, 1780, 1790, 1810, 1820, 1840, 1850, 1870, 1880, 1900, 1920, 1930, 1950, 1970, 1990, 2000, 2020, 2040, 2060, 2080, 2090, 2110, 2130, 2150, 2170, 2190, 2210, 2230, 2260, 2280, 2300, 2330, 2350, 2370, 2390] #newSAT 1160 + 10x
 
 def main():
 	heroku_conn = heroku3.from_key("624600ff-f687-4e19-a048-85304ad49803")
@@ -107,19 +110,19 @@ def process_sub(comment):
 							highNum = SATscores[theIndex + 1] - 10		
 							theType = "ACT"
 							notTheType = "SAT"
-							response = "between " + str(lowNum) + " and " + str(highNum)
+							response = "between " + str(lowNum) + " and " + str(highNum) 
 							print(response)
-							reply_text = REPLY_TEMP + theType + REPLY_TEMP2 + notTheType + REPLY_TEMP3 + response + REPLY_TEMP4
+							reply_text = REPLY_TEMP + notTheType + REPLY_TEMP2 + theType + REPLY_TEMP3 + response + REPLY_TEMP4
 						elif theNum == 36: #PERFECT ACT
 							theType = "ACT"
 							notTheType = "SAT"
 							response = "1600"
-							reply_text = REPLY_TEMP + theType + REPLY_TEMP2 + notTheType + REPLY_TEMP3 + response + REPLY_TEMP4
+							reply_text = REPLY_TEMP + notTheType + REPLY_TEMP2 + theType + REPLY_TEMP3 + response + REPLY_TEMP4
 						elif theNum == 1600: #PERFECT SAT
 							theType = "SAT"
 							notTheType = "ACT"
 							response = "36"
-							reply_text = REPLY_TEMP + theType + REPLY_TEMP2 + notTheType + REPLY_TEMP3 + response + REPLY_TEMP4
+							reply_text = REPLY_TEMP + notTheType + REPLY_TEMP2 + theType + REPLY_TEMP3 + response + REPLY_TEMP4
 						elif theNum >= 560 and theNum < 1600: #SAT provided
 							nearScore = min(SATscores, key=lambda x:abs(x - theNum))
 							print(nearScore)
@@ -127,11 +130,37 @@ def process_sub(comment):
 							if nearScore > theNum:
 								theIndex = theIndex - 1
 							ACT = ACTscores[theIndex]	
-							theType = "SAT"
+							theType = "new SAT"
 							notTheType = "ACT"
 							response = str(ACT)
 							print(response)
-							reply_text = REPLY_TEMP + theType + REPLY_TEMP2 + notTheType + REPLY_TEMP3 + response + REPLY_TEMP4
+							reply_text = REPLY_TEMP + notTheType + REPLY_TEMP2 + theType + REPLY_TEMP3 + response + REPLY_TEMP4
+						elif theNum == 2400: #Perfect old SAT
+							theType = "old SAT"
+							notTheType = "ACT"
+							notTheType2 = "new SAT"
+							response = "36"
+							response2 = "1600"
+							reply_text = REPLY_TEMP + notTheType + REPLY_TEMP2 + theType + REPLY_TEMP3 + response + REPLY_TEMP5 + notTheType2 + REPLY_TEMP2 + theType + REPLY_TEMP3 + response2 + REPLY_TEMP4
+						elif theNum >= 1610 and theNum <= 2390: #Old SAT
+							theType = "old SAT"
+							notTheType = "ACT"
+							notTheType2 = "new SAT"
+
+							nearScore = min(oldSATscores, key=lambda x:abs(x - theNum))
+							theIndex = SATscores.index(nearScore)
+							if nearScore > theNum:
+								theIndex = theIndex - 1
+							response2 = 1160 + 10 * theIndex #New SAT score
+
+							nearScore2 = min(SATscores, key=lambda x:abs(x - response2))
+							theIndex2 = SATscores.index(nearScore2)
+							if nearScore2 > response2:
+								theIndex2 = theIndex2 - 1
+							response = ACTscores[theIndex2] #ACT score
+
+							reply_text = REPLY_TEMP + notTheType + REPLY_TEMP2 + theType + REPLY_TEMP3 + response + REPLY_TEMP5 + notTheType2 + REPLY_TEMP2 + theType + REPLY_TEMP3 + response2 + REPLY_TEMP4
+
 						else: 
 							print("Invalid number provided")
 							theType = "invalid"
